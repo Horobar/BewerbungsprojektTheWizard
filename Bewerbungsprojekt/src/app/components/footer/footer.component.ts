feac54a44021ae3d137c487df718cc6d5ad74014
horobar <horobar19@outlook.com>
2021-05-10 08:05:55 +0200

    Footer 1.0

--- Bewerbungsprojekt/src/app/components/footer/footer.component.ts
+++ Bewerbungsprojekt/src/app/components/footer/footer.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  visitTime: string = 'Current Date and Time'; //Date();
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
