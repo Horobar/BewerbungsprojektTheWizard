bea79af698753cb8589441e8ca93f2a7b2c058ba
horobar <horobar19@outlook.com>
2021-05-10 08:06:30 +0200

    Header 1.0

--- Bewerbungsprojekt/src/app/components/header/header.component.ts
+++ Bewerbungsprojekt/src/app/components/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-
+  title: string = 'this will be a fancy project';
   constructor() { }
 
   ngOnInit(): void {
