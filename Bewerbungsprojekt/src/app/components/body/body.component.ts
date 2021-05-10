acba9ebdf4d84f855e5ada373e47aa29bdb6bc53
horobar <horobar19@outlook.com>
2021-05-10 11:55:54 +0200

    Stuff oneFile

--- Bewerbungsprojekt/src/app/components/body/body.component.ts
+++ Bewerbungsprojekt/src/app/components/body/body.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import { GetInputService } from '../../services/get-input.service';
 
 @Component({
   selector: 'app-body',
--- BewerbungsprojektOneComponent/ProjektOneComponent/src/app/app.component.ts
+++ BewerbungsprojektOneComponent/ProjektOneComponent/src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+//import { isNumeric } from 'rxjs/util/isNumeric';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ProjektOneComponent';
-}
+
+export class AppComponent
+{
+  title: string = 'this will be a fancy project';
+  output: string = 'Here will be the recentOutput';
+
+  let outputGain!:string;
+
+  //InputValue: string = "";
+
+  TestParam1: any = "1";
+  TestParam2: any = "1";
+
+  outputGain = this.add(this.TestParam1, this.TestParam2);
+  /**
+   * Summ two Params
+   */
+  public add(param1:any, param2: any)
+  {
+    if(isNaN(param1) || isNaN(param2))
+    {2
+      return 'Just Numbers please';
+    } else {
+      return Number(param1) + Number(param2);
+    }
+
+  }
